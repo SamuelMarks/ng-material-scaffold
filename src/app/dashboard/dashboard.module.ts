dd0ce521c94ee1d77ef8f980a19d04d53d3ff639
sc <sc@w.pl>
2022-07-23 02:48:31 +0200

    WIP: table drag and drop

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -7,21 +7,36 @@ import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { dashboardRoutes } from './dashboard.routes';
 import { DashboardComponent } from './dashboard.component';
-
+import { ItemSourceComponent } from './item-source/item-source.component';
+import { MatTableModule } from '@angular/material/table';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { ItemGroupsComponent } from './item-groups/item-groups.component';
+import { GroupComponent } from './item-groups/group/group.component';
 
 @NgModule({
   imports: [
-    CommonModule, RouterModule, RouterModule.forChild(dashboardRoutes),
+    CommonModule,
+    RouterModule,
+    RouterModule.forChild(dashboardRoutes),
     MatGridListModule,
     MatCardModule,
     MatMenuModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    DragDropModule,
+    MatCheckboxModule,
   ],
-  declarations: [DashboardComponent],
-  exports: [DashboardComponent]
+  declarations: [
+    DashboardComponent,
+    ItemSourceComponent,
+    ItemGroupsComponent,
+    GroupComponent,
+  ],
+  exports: [DashboardComponent],
 })
 export class DashboardModule {}
---
+++ src/app/dashboard/item-groups/item-groups.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ItemGroupsComponent } from './item-groups.component';
+
+describe('ItemGroupsComponent', () => {
+  let component: ItemGroupsComponent;
+  let fixture: ComponentFixture<ItemGroupsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ItemGroupsComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ItemGroupsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
